00bf0d2ba97a2866bcf487c10c51e4a9fdee5d17
DavidVelascoHerrero1 <david.velasco@tecozam.com>
2022-06-07 11:21:49 +0200

    Consumo y Tarifa
    Creación nuevas entidades

--- Expertis.Business.Vehiculos/Consumo.vb
+++ Expertis.Business.Vehiculos/Consumo.vb
@@ -2,7 +2,7 @@
 Imports Solmicro.Expertis.Engine.BE.BusinessProcesses
 Imports Solmicro.Expertis.Engine.DAL
 
-Public Class Vehiculo
+Public Class Consumo
 
     Inherits Solmicro.Expertis.Engine.BE.BusinessHelper
 
@@ -10,7 +10,7 @@ Public Class Vehiculo
         MyBase.New(cnEntidad)
     End Sub
 
-    Private Const cnEntidad As String = "tbMaestroVehiculos"
+    Private Const cnEntidad As String = "tbVehiculoConsumo"
 
     Protected Overrides Sub RegisterAddnewTasks(ByVal addnewProcess As Engine.BE.BusinessProcesses.Process)
         MyBase.RegisterAddnewTasks(addnewProcess)
@@ -19,23 +19,17 @@ Public Class Vehiculo
 
     <Task()> Public Shared Sub FillDefaultValues(ByVal data As DataRow, ByVal services As ServiceProvider)
         ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarValoresPredeterminados, data, services)
-        'ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarCentroGestion, data, services)
-        'ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarContador, data, services)
+
     End Sub
 
-
     <Task()> Public Shared Sub AsignarValoresPredeterminados(ByVal data As DataRow, ByVal services As ServiceProvider)
-        data("idVehiculo") = AdminData.GetAutoNumeric
-        'Dim dt As New DataTable
-        'dt = AdminData.GetData("SELECT MAX(IDVehiculo) As ID FROM tbMaestroVehiculos")
-
-        'data("idVehiculo") = dt(0)(0) + 1
-
+        data("IDConsumo") = AdminData.GetAutoNumeric
     End Sub
 
     Protected Overrides Sub RegisterUpdateTasks(ByVal updateProcess As Engine.BE.BusinessProcesses.Process)
         MyBase.RegisterUpdateTasks(updateProcess)
-        'updateProcess.AddTask(Of DataRow)(AddressOf AsignarValoresPredeterminados)
+        updateProcess.AddTask(Of DataRow)(AddressOf AsignarValoresPredeterminados)
     End Sub
+
 
 End Class
---
+++ Expertis.Business.Vehiculos/EmpresaTipoPrecios.vb
@@ -0,0 +1,33 @@
+﻿Imports Solmicro.Expertis
+Imports Solmicro.Expertis.Engine.BE.BusinessProcesses
+Imports Solmicro.Expertis.Engine.DAL
+
+Public Class EmpresaTipoPrecios
+    Inherits Solmicro.Expertis.Engine.BE.BusinessHelper
+
+    Public Sub New()
+        MyBase.New(cnEntidad)
+    End Sub
+
+    Private Const cnEntidad As String = "tbVehiculoTarifa"
+
+    Protected Overrides Sub RegisterAddnewTasks(ByVal addnewProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterAddnewTasks(addnewProcess)
+        'addnewProcess.AddTask(Of DataRow)(AddressOf FillDefaultValues)
+    End Sub
+    '<Task()> Public Shared Sub FillDefaultValues(ByVal data As DataRow, ByVal services As ServiceProvider)
+    '    ProcessServer.ExecuteTask(Of DataRow)(AddressOf AsignarValoresPredeterminados, data, services)
+
+    'End Sub
+    '<Task()> Public Shared Sub AsignarValoresPredeterminados(ByVal data As DataRow, ByVal services As ServiceProvider)
+    '    data("IDTarifa") = AdminData.GetAutoNumeric
+
+
+    'End Sub
+    Protected Overrides Sub RegisterUpdateTasks(ByVal updateProcess As Engine.BE.BusinessProcesses.Process)
+        MyBase.RegisterUpdateTasks(updateProcess)
+    End Sub
+    Public Function devuelveAuto() As Integer
+        Return AdminData.GetAutoNumeric
+    End Function
+End Class
